953cc9e501a787b16669577517236724fc5d378f
Satheesh Kannan <satheesh@okaythis.com>
2023-09-21 00:27:46 +0530

    30 Days of Code :: Day 0 :: Hello, World.

---
+++ 30_Days_of_Code/Day_00_Hello_World.swift
@@ -0,0 +1,23 @@
+//
+//  Day_00_Hello_World.swift
+//  
+//
+//  Created by Satheesh Kannan on 21/09/23.
+//
+
+/// Source: https://www.hackerrank.com/challenges/30-hello-world/problem
+///
+/// To complete this challenge,
+///     - You must save a line of input from stdin to a variable
+///     - Print Hello, World. on a single line
+///     - Finally print the value of your variable on a second line.
+
+import Foundation
+
+let inputString = readLine()! // get a line of input from stdin and save it to our variable
+
+// Your first line of output goes here
+print("Hello, World.")
+
+// Write the second line of output
+print(inputString)
