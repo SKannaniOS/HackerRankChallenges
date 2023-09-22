c507d66ca3d4be9caa5ba048c1558c3b64ffd2c5
Satheesh Kannan <satheesh@okaythis.com>
2023-09-22 23:53:59 +0530

    30 Days of Code :: Day 2 :: Operators

---
+++ 30_Days_of_Code/Day_02_Operators.swift
@@ -0,0 +1,21 @@
+//
+//  Day_02_Operators.swift
+//  
+//
+//  Created by SK on 22/09/23.
+//
+
+///Source: https://www.hackerrank.com/challenges/30-operators/problem
+
+import Foundation
+
+///Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip), and tax percent (the percentage of the meal price being added as tax) for a meal, find and print the meal's total cost. Round the result to the nearest integer.
+                                                            
+func solve(meal_cost: Double, tip_percent: Int, tax_percent: Int) -> Void {
+    let tipAmount: Double = (Double(tip_percent) / 100.0) * meal_cost
+    let taxAmount: Double = (Double(tax_percent) / 100.0) * meal_cost
+    
+    let totalCost = meal_cost + tipAmount + taxAmount
+    
+    print(Int(totalCost.rounded()))
+}
