fbf73fccd54aa9137c75c41750391a308d143e4e
Satheesh Kannan <satheesh@okaythis.com>
2023-09-26 23:50:03 +0530

    30 Days of Code :: Day 6 :: Let's Review

---
+++ 30_Days_of_Code/Day_06_Let's_Review.swift
@@ -0,0 +1,24 @@
+//
+//  Day_06_Let's_Review.swift
+//  
+//
+//  Created by SK on 26/09/23.
+//
+
+/// Source: https://www.hackerrank.com/challenges/30-review-loop/problem
+
+import Foundation
+
+/// Task:
+/// Given a string S, of N length that is indexed from 0 to N-1,
+/// print its even-indexed and odd-indexed characters as 2 space-separated strings
+/// on a single line
+
+func printEvenAndOdd(string: String) {
+    fputs("string: " + string + "\n", stderr)
+    
+    let evenString = string.enumerated().filter({ $0.0 % 2 == 0 }).map({ String($0.element) }).joined()
+    let oddString = string.enumerated().filter({ $0.0 % 2 != 0 }).map({ String($0.element) }).joined()
+    
+    print("\(evenString) \(oddString)")
+}
