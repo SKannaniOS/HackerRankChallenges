6cd7f24bff3f45cb51407ff75b93e226d9d6876b
Satheesh Kannan <satheesh@okaythis.com>
2023-10-06 00:36:47 +0530

    30 Days of Code :: Day 10 :: Binary Numbers

---
+++ 30_Days_of_Code/Day_10_Binary_Numbers.swift
@@ -0,0 +1,20 @@
+//
+//  Day_10_Binary_Numbers.swift
+//  
+//
+//  Created by SK on 06/10/23.
+//
+
+/// Source: https://www.hackerrank.com/challenges/30-binary-numbers/problem
+
+import Foundation
+
+/// Task:
+/// Given a base-10 integer, n, convert it to binary (base-2).
+/// Then find and print the base-10 integer denoting the maximum number of consecutive 1's in n's binary representation. When working with different bases, it is common to show the base as a subscript.
+
+guard let n = Int((readLine()?.trimmingCharacters(in: .whitespacesAndNewlines))!)
+else { fatalError("Bad input") }
+
+let result = String(n, radix: 2).split(separator: "0").sorted().last!.count
+print(result)
