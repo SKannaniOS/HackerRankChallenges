147252773868abd69cce6e806b8731f4f95b2742
Satheesh Kannan <satheesh@okaythis.com>
2023-10-06 00:18:19 +0530

    30 Days of Code :: Day 9 :: Recursion 3

---
+++ 30_Days_of_Code/Day_09_Recursion_3.swift
@@ -0,0 +1,27 @@
+//
+//  Day_09_Recursion_3.swift
+//  
+//
+//  Created by SK on 06/10/23.
+//
+
+/// Source: https://www.hackerrank.com/challenges/30-recursion/problem
+
+import Foundation
+
+/// Task:
+/// Complete the factorial function in the editor below. Be sure to use recursion.
+/// factorial has the following paramter:
+///     int n: an integer
+/// Returns
+///     int: the factorial of n
+/// Note: If you fail to use recursion or fail to name your recursive function factorial or Factorial, you will get a score of 0.
+
+let n = Int(readLine()!)!
+
+func factorial(n: Int) -> Int {
+    guard n != 1 else { return n }
+    return n * factorial(n: n - 1)
+}
+
+print(factorial(of: n))
