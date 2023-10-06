382eff1bac62c15e20b1c5f36357846bc9631c3e
Satheesh Kannan <satheesh@okaythis.com>
2023-10-07 00:02:20 +0530

    30 Days of Code :: Day 11 :: 2D Arrays

---
+++ 30_Days_of_Code/Day_11_2D_Arrays.swift
@@ -0,0 +1,56 @@
+//
+//  Day_11_2D_Arrays.swift
+//  
+//
+//  Created by SK on 06/10/23.
+//
+
+/// Source: https://www.hackerrank.com/challenges/30-2d-arrays/problem
+
+import Foundation
+
+/// Task:
+/// Calculate the hourglass sum for every hourglass in A, then print the maximum hourglass sum.
+
+/// Samlpe: A
+///
+/// [[1, 1, 1, 0, 0, 0],
+/// [0, 1, 0, 0, 0, 0],
+/// [1, 1, 1, 0, 0, 0],
+/// [0, 0, 2, 4, 4, 0],
+/// [0, 0, 0, 2, 0, 0],
+/// [0, 0, 1, 2, 4, 0]]
+///
+/// Answer: 19
+
+
+var arr = [[Int]]()
+
+for _ in 1...6 {
+    guard let arrRowTemp = readLine()?.replacingOccurrences(of: "\\s+$", with: "", options: .regularExpression) else { fatalError("Bad input") }
+
+    let arrRow: [Int] = arrRowTemp.split(separator: " ").map {
+        if let arrItem = Int($0) {
+            return arrItem
+        } else { fatalError("Bad input") }
+    }
+
+    guard arrRow.count == 6 else { fatalError("Bad input") }
+
+    arr.append(arrRow)
+}
+
+guard arr.count == 6 else { fatalError("Bad input") }
+
+// Solution starts below
+
+var allValues = [Int]()
+for i in 0...3 {
+    for j in 0...3 {
+        let value = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
+        allValues.append(value)
+    }
+}
+
+print(allValues.sorted().last!)
+
---
+++ 30_Days_of_Code/Day_12_Inheritance.swift
@@ -0,0 +1,8 @@
+//
+//  Day_12_Inheritance.swift
+//  
+//
+//  Created by SK on 06/10/23.
+//
+
+import Foundation
