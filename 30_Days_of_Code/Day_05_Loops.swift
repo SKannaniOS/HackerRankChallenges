2a940754d1fd3e48528563ec33ac0f5b5b2260cd
Satheesh Kannan <satheesh@okaythis.com>
2023-10-19 01:03:04 +0530

    30 Days of Code :: Day 5 :: Loops

--- 30_Days_of_Code/Day_05_Loops.swift
+++ 30_Days_of_Code/Day_05_Loops.swift
@@ -6,7 +6,7 @@
 //
 
 /// Source: https://www.hackerrank.com/challenges/30-loops/problem
-///
+
 /// Task:
 /// Given an integer n, print its first 10 multiples. 
 /// Each multiple n x i (where i 1...10) should be printed on a new line in the form:
